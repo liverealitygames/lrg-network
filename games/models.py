c6bdf3e61dd09b0df7c17c47958bc3a0bb58795e
Austin Gardner <austintgardner@gmail.com>
2025-04-29 16:48:19 -0700

    Fix slug generation

--- games/models.py
+++ games/models.py
@@ -71,7 +71,7 @@ class Game(CoreModel):
         base_slug = slugify(self.name)
         slug = base_slug
         counter = 1
-        while Game.objects.filter(slug=slug).exists():
+        while Game.objects.filter(slug=slug).exclude(pk=self.pk).exists():
             slug = f"{base_slug}-{counter}"
             counter += 1
         self.slug = slug
