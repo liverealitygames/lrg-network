dee10cf137861666afdfa1d534aeae8aa415a5a7
Austin Gardner <austintgardner@gmail.com>
2025-04-29 15:25:33 -0700

    Set up basic view for game list

--- games/views.py
+++ games/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from games.models import Game
+
+
 # Create your views here.
+def game_list(request):
+    games = Game.objects.all()
+    return render(request, "games/game_list.html", {"games": games})
