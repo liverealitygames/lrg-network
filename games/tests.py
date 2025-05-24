5fdcf0a05c1fc4128170da60fe7d07056fa67998
Austin Gardner <austintgardner@gmail.com>
2025-05-23 17:34:57 -0700

    Update test for region name

--- games/tests.py
+++ games/tests.py
@@ -166,7 +166,7 @@ class GameLocationDisplayTests(TestCase):
         game = Game.objects.create(
             name="Test Game", country=self.country, region=self.region, city=self.city
         )
-        self.assertEqual(game.location_display(), "Los Angeles, CA")
+        self.assertEqual(game.location_display(), "Los Angeles, California")
 
     def test_location_display_with_region_country(self):
         game = Game.objects.create(
