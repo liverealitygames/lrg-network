6a35b0e52edf4595ba7979b42d5c8f73ee7ed927
Austin Gardner <austintgardner@gmail.com>
2025-05-23 17:45:02 -0700

    Paginate games

--- games/views.py
+++ games/views.py
@@ -1,11 +1,15 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from games.models import Game
 
 
 def game_list(request):
-    games = Game.objects.all()
-    return render(request, "games/game_list.html", {"games": games})
+    game_queryset = Game.objects.all().order_by("name")  # or whatever ordering
+    paginator = Paginator(game_queryset, 10)  # 10 games per page
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, "games/game_list.html", {"page_obj": page_obj})
 
 
 def game_detail(request, slug):
