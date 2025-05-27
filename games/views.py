1d8a6f0333a7b6a90b9d2c4791844fa7604bf880
Austin Gardner <austintgardner@gmail.com>
2025-05-26 18:22:04 -0700

    Add search (#21)
    Add a basic search (on game name and description) 
    
    
    https://github.com/user-attachments/assets/e481253a-51c8-4d35-9900-3f1f296df915

--- games/views.py
+++ games/views.py
@@ -1,15 +1,29 @@
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
 
 from games.models import Game
 
 
 def game_list(request):
-    game_queryset = Game.objects.all().order_by("name")  # or whatever ordering
-    paginator = Paginator(game_queryset, 10)  # 10 games per page
+    query = request.GET.get("q", "")
+    games = Game.objects.all().order_by("name")
+
+    if query:
+        games = games.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    paginator = Paginator(games, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    return render(request, "games/game_list.html", {"page_obj": page_obj})
+
+    return render(
+        request,
+        "games/game_list.html",
+        {
+            "page_obj": page_obj,
+            "query": query,
+        },
+    )
 
 
 def game_detail(request, slug):
