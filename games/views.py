a3d9cc5621e44c1a5ccb21348b7efafe81f35b97
Austin Gardner <austintgardner@gmail.com>
2025-06-10 16:05:31 -0700

    Add game format filtering

--- games/views.py
+++ games/views.py
@@ -7,14 +7,21 @@ from games.models import Game
 
 def game_list(request):
     query = request.GET.get("q", "")
+    game_format = request.GET.get("game_format")
+
     games = Game.objects.all().order_by("name")
 
     if query:
         games = games.filter(Q(name__icontains=query) | Q(description__icontains=query))
 
+    if game_format:
+        games = games.filter(game_format=game_format)
+
     paginator = Paginator(games, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+
+    game_formats = Game.GameFormat.choices
 
     return render(
         request,
@@ -22,6 +29,8 @@ def game_list(request):
         {
             "page_obj": page_obj,
             "query": query,
+            "game_formats": game_formats,
+            "selected_game_format": game_format,
         },
     )
 
