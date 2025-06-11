fa2d50507acdced5138b41357225f728a3c611ec
Austin Gardner <austintgardner@gmail.com>
2025-06-10 17:19:39 -0700

    Add location filters

--- games/views.py
+++ games/views.py
@@ -1,3 +1,4 @@
+from cities_light.models import Country, Region, City
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render
@@ -14,6 +15,9 @@ def game_list(request):
     only_for_charity = request.GET.get("only_for_charity") == "on"
     include_college_games = request.GET.get("include_college_games") == "on"
     include_friends_and_family = request.GET.get("include_friends_and_family") == "on"
+    country_id = request.GET.get("country")
+    region_id = request.GET.get("region")
+    city_id = request.GET.get("city")
 
     games = Game.objects.all().order_by("name")
 
@@ -43,6 +47,15 @@ def game_list(request):
             Q(friends_and_family=False) | Q(friends_and_family__isnull=True)
         )
 
+    if country_id:
+        games = games.filter(country_id=country_id)
+
+    if region_id:
+        games = games.filter(region_id=region_id)
+
+    if city_id:
+        games = games.filter(city_id=city_id)
+
     paginator = Paginator(games, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -50,6 +63,9 @@ def game_list(request):
     game_formats = Game.GameFormat.choices
     game_durations = Game.GameDuration.choices
     filming_statuses = Game.FilmingStatus.choices
+    countries = Country.objects.all()
+    regions = Region.objects.filter(country_id=country_id) if country_id else []
+    cities = City.objects.filter(region_id=region_id) if region_id else []
 
     return render(
         request,
@@ -60,6 +76,12 @@ def game_list(request):
             "game_formats": game_formats,
             "game_durations": game_durations,
             "filming_statuses": filming_statuses,
+            "countries": countries,
+            "regions": regions,
+            "cities": cities,
+            "selected_country": country_id,
+            "selected_region": region_id,
+            "selected_city": city_id,
             "selected_game_format": game_format,
             "selected_game_duration": game_duration,
             "selected_filming_status": filming_status,
