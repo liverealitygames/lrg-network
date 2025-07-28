7675f20326f9267680124cde3ea3ea3bc91b6ff7
Austin Gardner <austintgardner@gmail.com>
2025-07-27 18:05:09 -0700

    Setup Django Axes for security (#33)
    Adds Django Axes for improved security (IP Addresses will be locked out
    after multiple failed attempts to login).

--- lrgnetwork/settings.py
+++ lrgnetwork/settings.py
@@ -41,6 +41,14 @@ INSTALLED_APPS = [
     "games",
     "compressor",
     "storages",
+    "axes",
+]
+
+AUTHENTICATION_BACKENDS = [
+    # AxesStandaloneBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
+    "axes.backends.AxesStandaloneBackend",
+    # Django ModelBackend is the default authentication backend.
+    "django.contrib.auth.backends.ModelBackend",
 ]
 
 MIDDLEWARE = [
@@ -52,6 +60,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # AxesMiddleware should be the last middleware in the MIDDLEWARE list.
+    # It only formats user lockout messages and renders Axes lockout responses
+    # on failed user authentication attempts from login views.
+    # If you do not want Axes to override the authentication response
+    # you can skip installing the middleware and use your own views.
+    "axes.middleware.AxesMiddleware",
 ]
 
 ROOT_URLCONF = "lrgnetwork.urls"
